e31216398de57c2e303e5035f9b674fe78c8d0c9
ZarckDev <zarck.dev@gmail.com>
2021-04-17 17:15:05 +0200

    + Improve Tour Model - Remove Check Body (forgot to remove before) + Add script to delete or import JSON dummy data

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,35 +4,44 @@ const Tour = require('../models/tourModel');
 
 
 
+// ROUTES HANDLERS
+exports.getAllTours = async(req, res) => {
+    try {
+        const tours = await Tour.find();
 
-exports.checkBody = (req, res, next) => {
-    if(!req.body.name || !req.body.price){
-        return res.status(400).json({
+        res.status(200).json({
+            status: 'success',
+            results: tours.length, // because we send an array
+            data:{
+                tours
+            }
+        })
+    } catch(err) {
+        res.status(404).json({
             status: 'fail',
-            message: 'Missing name or price'
+            message: err
         })
     }
-    next();
 }
 
+exports.getTour = async(req, res) => { 
+    const { id } = req.params;
+    try{
+        const tour = await Tour.findById(id);
+        // Tour.findOne({ _id: req.params.id })
 
-// ROUTES HANDLERS
-exports.getAllTours = (req, res) => {
-    // console.log(req.requestTime) // from middleware Time
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        // results: toursData.length, // because we send an array
-        // data:{
-        //     tours: toursData
-        // }
-    })
-}
-
-exports.getTour = (req, res) => { // :y?   -> ? for optional param
-    const id = req.params.id * 1;// convert to Number
-
-    // const tour = toursData.find(el => el.id === id)
+        res.status(200).json({
+            status: 'success',
+            data:{
+                tour
+            }
+        })
+    } catch(err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
 
     // //otherwise
     // res.status(200).json({
@@ -44,29 +53,62 @@ exports.getTour = (req, res) => { // :y?   -> ? for optional param
 
 }
 
-exports.createTour = (req, res) => {
-    res.status(201).json({// created
-        status: 'success',
-        // data: {
-        //     tour: newTour
-        // }
-    }); 
+exports.createTour = async(req, res) => {
+    try {
+        //const NewTour = new Tour({})
+        // await newTour.save()
+        const newTour = await Tour.create({...req.body});
+        
+        res.status(201).json({// created
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        }); 
+    } catch(err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        })
+    }
 }
 
-exports.updateTour = (req, res) => {
-    //not implemented 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated tour here...>'
-        }
-    })
+exports.updateTour = async(req, res) => {
+    const { id } = req.params;
+    try{
+        const tour = await Tour.findByIdAndUpdate(id, {...req.body}, {
+            new: true, // return the new document into "tour"
+            runValidators: true // on update we check the Schema, default is false
+        });
+        // Tour.findOne({ _id: req.params.id })
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        })
+    } catch(err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
 }
 
-exports.deleteTour = (req, res) => {
-    //not implemented 
-    res.status(204).json({ // 204 No Content
-        status: 'success',
-        data: null // show the ressource we deleted is not enymore here
-    })
+exports.deleteTour = async(req, res) => {
+    const { id } = req.params;
+    try{
+        await Tour.findByIdAndDelete(id);
+
+        res.status(204).json({ // 204 No Content
+            status: 'success',
+            data: null // show the ressource we deleted is not enymore here
+        })
+    } catch(err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }  
 }
