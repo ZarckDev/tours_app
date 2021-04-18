319e817e973bee923ffdb1d1c7c80c1ff47601e2
ZarckDev <zarck.dev@gmail.com>
2021-04-18 12:13:42 +0200

    + Document middleware, pre() post() on model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const slugify = require('slugify')
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -51,12 +52,14 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(), // Mongo converts automatically to string date
         select: false // when we try to select (in fields limiting), we just hide createdAt
     },
-    startDates: [Date]
+    startDates: [Date],
+    slug: String // for our pre
 }, {
     toJSON: { virtuals: true }, // virtuals to be part when outputing as JSON
     toObject: { virtuals: true }, // virtuals to be part when outputing as Object
+    id: false // remove the additional id created with pre() and post()
 });
-const Tour = mongoose.model('Tour', tourSchema)
+
 
 //Virtual Properties -> not stored in Database
 tourSchema.virtual('durationWeeks').get(function () {// get because will be created each time we get data from the database
@@ -64,5 +67,25 @@ tourSchema.virtual('durationWeeks').get(function () {// get because will be crea
     return this.duration / 7; // duration in weeks instead of days
 }) // we cannot use virtuals in a Query ! Because they are not part of the Database
 
+
+//Middleware on Documents -- allow us to do something before or after an operation on document
+tourSchema.pre('save', function(next) {// run BEFORE an event on this model (here, before 'save()' and 'create()' in DB)
+    // console.log(this); // this points to the current processed document
+    this.slug = slugify(this.name, {lower:true})//new property
+    next();
+}) 
+
+// tourSchema.pre('save', function(next) { // we can have multiple pre() for a same "Hook"
+//     console.log('Will save document...');
+//     next();
+// })
+
+// tourSchema.post('save', function(doc, next) {
+//     // we have the finished document here
+//     console.log(doc);
+//     next(); // not really needed but good practice
+// })
+
+const Tour = mongoose.model('Tour', tourSchema)
 
 module.exports = Tour;
