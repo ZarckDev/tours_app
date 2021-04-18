a95d5d983b5fcc21dda6cb3db4e9a40500689bd2
ZarckDev <zarck.dev@gmail.com>
2021-04-18 23:37:08 +0200

    + Non express errors handling + Uncaught Exception and Unhandled rejection

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ dotenv.config({path: './config.env'});
 
 const mongoose = require('mongoose')
 
-const app = require('./app')
+//UNCAUGHT EXCEPTION - something that does not exists for eample -- should be on top of everything
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...')
+    console.log(err.name, err.message);
+    // We need to crash the app, if not, unclean state
+    process.exit(1);
 
+})
+// console.log(x) // test for uncaught exception
+
+
+const app = require('./app')
 
 const DB_URL = process.env.MONGODB_URL.replace('<password>', process.env.MONGODB_PWD);
 mongoose.connect(DB_URL, {
@@ -14,14 +24,27 @@ mongoose.connect(DB_URL, {
     useFindAndModify: false,
     useUnifiedTopology: true
 }).then(conn => console.log('DB connection successful'))
-//TODO Add  catch
+//TODO Add  catch - Unhandled Promise rejection if we don't manage error of MongoDB
+// We could add catch() BUT better to generally manage the "Unhandled" rejection" as global event -- SEE BELOW ( maybe better to add catch alors)
 
 
 
 
 // START SERVER
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
+})
+
+// SHOULD WE USE THE HANDLE BELOW ? OR MANAGING ERRORS AT THE RIGHT SPOTS
+
+// HANDLE UNHANDLED REJECTION -- Example: bad authentification connection MongoDB
+process.on('unhandledRejection', err => { // any Promise rejection is handled here
+    console.log('UNHANDLED REJECTION! 💥 Shutting down...')
+    console.log(err.name, err.message);
+    // close safely the server
+    server.close(() => { // finish all the request pending before exiting 
+        process.exit(1); // shutdown anyway - 1 Uncaught exception (0 for success)
+    })
 })
 
