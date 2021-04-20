0281baa99336c18839101de7e56c6594c1e41866
ZarckDev <zarck.dev@gmail.com>
2021-04-20 20:10:01 +0200

    + add helmet package for HTTP Headers protection + limit the request body amount of data

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 
 const express = require('express');
 const morgan = require('morgan'); // Request Logger with details
+
+//Security
 const rateLimit = require('express-rate-limit')
+//HELMET HTTP header security
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 
@@ -14,10 +18,15 @@ const app = express();
 
 
 // GLOBAL MIDDLEWARES - for all the routes
+
+// Devepment logging
 console.log(process.env.NODE_ENV)
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
+
+//Use helmet to protect HTTP Header
+app.use(helmet());// issue with contentSecurityPolicy() which is set by default
 
 //limite the requests (if atttacks -- Brute force)
 const limiter = rateLimit({
@@ -27,12 +36,15 @@ const limiter = rateLimit({
 })
 app.use('/api', limiter) // all routes that starts with '/api'
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ // options to limit the amount of data sent in the body for SECURITY
+    limit: '10kb' // limit of 10k bytes
+}));
 
 // Define the route for the Public folder to be accessible
 app.use(express.static(`${__dirname}/public`))
 
-// middleware
+// Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString(); // if we want the time for every request
     // console.log(req.headers);
