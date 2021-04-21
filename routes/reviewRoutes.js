48355577e5e54a154e2d144efc6d7a62cd2a7134
ZarckDev <zarck.dev@gmail.com>
2021-04-21 13:37:32 +0200

    + keep also the /reviews as a route for post/get

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,8 +6,15 @@ const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true }); // get the params from URL defined previously (in tourRoutes for :tourId access)
 
+// FOR THOSE ROUTES :
+// POST /tours/246456/reviews
+// POST /reviews
+
 
 // REVIEWS
 router.route('/') ///api/v1/tours/:tourId/reviews
     .get(reviewController.getAllReviews) // get all reviews for this tour
     .post(authController.protect, authController.restrictTo('user'), reviewController.createReview) // only user can post a review
+
+
+module.exports = router
