8d350ddc98395583f5ef2ed2176e864be6daa6e2
ZarckDev <zarck.dev@gmail.com>
2021-04-25 19:24:01 +0200

    + API Booking Routes

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -59,3 +59,11 @@ exports.createBookingCheckout = catchAsync(async(req, res, next) => {
     // redirect to the url WITHOUT the query string
     res.redirect(req.originalUrl.split('?')[0]); // remove the query string
 })
+
+
+// API
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -6,8 +6,22 @@ const authController = require('../controllers/authController');
 
 const router = express.Router(); 
 
+// protect all following routes
+router.use(authController.protect)
 
-router.get('/checkout-session/:tourId', authController.protect, bookingController.getCheckoutSession)
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession)
 
+
+// Admin or lead-guide only
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router.route('/')
+    .get(bookingController.getAllBookings)
+    .post(bookingController.createBooking)
+
+router.route('/:id')
+    .get(bookingController.getBooking)
+    .patch(bookingController.updateBooking)
+    .delete(bookingController.deleteBooking)
 
 module.exports = router
